cad8a27e650b46bd994010459c0aaa129df215dd
Pavel Kaplin <pavel.kaplin@teamdev.com>
2012-06-19 12:40:58 -0300

    Initial state

---
+++ src/com/teamdev/appengine/meter/AppEngineCostReport.java
@@ -0,0 +1,32 @@
+package com.teamdev.appengine.meter;
+
+import org.apache.jmeter.samplers.SampleResult;
+import org.apache.jmeter.visualizers.gui.AbstractVisualizer;
+
+public class AppEngineCostReport extends AbstractVisualizer {
+
+	@Override
+	public void add(SampleResult sample) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void clearData() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getLabelResource() {
+		return null;
+	}
+
+	@Override
+	public String getStaticLabel() {
+		return "App Engine Cost Report";
+	}
+	
+	
+
+}
